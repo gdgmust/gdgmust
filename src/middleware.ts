0a579bc30434d8d639ae8939437dbf5639e4ad67
roaziy <erkhemtur389@gmail.com>
2025-07-27 20:54:51 +0800

    hackathon

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,21 +8,23 @@ const intlMiddleware = createMiddleware(routing);
 
 // Export a wrapped middleware function
 export default function middleware(request: NextRequest) {
-  // Get the response from the intl middleware
-  const response = intlMiddleware(request);
-  
-  if (response) {
-    // Add headers to encourage opening links in system browser instead of in-app browsers
-    response.headers.set('X-Frame-Options', 'DENY');
-    response.headers.set('X-Content-Type-Options', 'nosniff');
-    
-    // Add header to instruct Facebook apps to use external browser
-    response.headers.set('Content-Security-Policy', 
-      "frame-ancestors 'self'; default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; connect-src 'self' https:; font-src 'self' data:; frame-src 'self' https:;");
-  }
-  
-  return response;
+  // Redirect all requests to the Facebook event page
+  return NextResponse.redirect('https://www.facebook.com/events/1473674620455226', 301);
 }
+
+// export default function middleware(request: NextRequest) {
+//   const response = intlMiddleware(request);
+  
+//   if (response) {
+//     response.headers.set('X-Frame-Options', 'DENY');
+//     response.headers.set('X-Content-Type-Options', 'nosniff');
+    
+//     response.headers.set('Content-Security-Policy', 
+//       "frame-ancestors 'self'; default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; connect-src 'self' https:; font-src 'self' data:; frame-src 'self' https:;");
+//   }
+  
+//   return response;
+// }
  
 export const config = {
   // Match all routes except for:
